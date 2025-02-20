4970f86a8a5baa4e02a7d065b455cc280eaf9f47
Daniel Comaduran <dcomadur@ucalgary.ca>
2025-02-20 16:08:17 -0700

    cpp file

--- embedded-cpp/embedded-cpp.ino
+++ embedded-cpp/embedded-cpp.ino
@@ -1,53 +1,16 @@
-#include <Arduino.h>
+#include "potentiometer.h"
 
-class ThresholdSensor {
-private:
-    const int analogPin;
-    const int digitalPin;
-    const int threshold;
-    int analogValue;
-    int digitalValue;
+int analogPin = A0;     // Analog pin connected to the potentiometer
+int ledPin = 13;        // Digital pin connected to the built-in LED
+int threshold = 512;    // Threshold value for the potentiometer
 
-public:
-    ThresholdSensor(int aPin, int dPin, int thresh) : 
-        analogPin(aPin), 
-        digitalPin(dPin), 
-        threshold(thresh),
-        analogValue(0),
-        digitalValue(0) {
-        pinMode(digitalPin, OUTPUT);
-    }
-
-    void read() {
-        analogValue = analogRead(analogPin);
-        digitalValue = (analogValue > threshold) ? 1023 : 0;
-        digitalWrite(digitalPin, (analogValue > threshold));
-    }
-
-    void printValues() {
-        Serial.print("analog:");
-        Serial.print(analogValue);
-        Serial.print(" digital:");
-        Serial.println(digitalValue);
-    }
-};
-
-ThresholdSensor sensor(A0, 2, 512);
+Potentiometer pot(A0, 13, 512);
 
 void setup() {
-    Serial.begin(9600);
-}
-
-void waitMillis(unsigned long interval) {
-    static unsigned long lastMillis = 0;
-    unsigned long currentMillis = millis();
-    if (currentMillis - lastMillis >= interval) {
-        lastMillis = currentMillis;
-    }
+    pot.begin();
 }
 
 void loop() {
-    sensor.read();
-    sensor.printValues();
-    waitMillis(10);
+    pot.update();
+    delay(100);
 }
